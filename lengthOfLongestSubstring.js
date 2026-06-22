d69cf0c553af38a3f731841e6ae7205b90ea70bb
uoyniXnaH <b4baker@gmail.com>
2026-06-22 20:46:07 +0900

    added Longest Substring Without Repeating Characters

---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,23 @@
+var lengthOfLongestSubstring = function(s) {
+    let ans = "";
+    let hold = "";
+    let pos = 0;
+    for (i = 0; i < s.length; i++) {
+        // console.log(`hold: ${hold}, ans: ${ans}`);
+        if (hold.includes(s[i])) {
+            pos = hold.indexOf(s[i]);
+            if (hold.length > ans.length) {
+                ans = hold;
+            }
+            hold = hold.slice(pos + 1) + s[i];
+        } else {
+            hold += s[i];
+        }
+    }
+    if (hold.length > ans.length) {
+        ans = hold;
+    }
+    return ans.length;
+};
+
+lengthOfLongestSubstring("pwwkew");
